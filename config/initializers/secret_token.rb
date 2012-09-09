6318ab9716c057fa8257233856f67535a82d64a5
Ana Rey <anarey@gmail.com>
2012-09-09 12:33:34 +0200

    initial commmit
    Creada paginas estaticas home, contact y about
    Creada estructura de datos de user y micropost

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pingupal::Application.config.secret_token = '73868301df38fce23a8a24114206933dc2353dd0d1cc75a3cb80ff85538541a2cfba626869a48c305fef11d43744db1157c6fc495e64f2ec5705e9da7119246a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pingupal::Application.config.session_store :cookie_store, key: '_pingupal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pingupal::Application.config.session_store :active_record_store
