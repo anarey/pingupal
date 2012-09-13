8c14b24a78fbe6ebdaf29c162e56bf83b166e5eb
Ana Rey <anarey@gmail.com>
2012-09-13 12:58:37 +0200

    Añadir campo password al modelo de datos

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
-
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
   validates :name,  presence: true, length: { maximum: 50 }
   
   before_save { |user| user.email = email.downcase }
@@ -19,6 +19,8 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
+  validates :password, presence: true, length: { minimum:6 }
+  validates :password_confirmation, presence: true
   has_many :microposts
   
 end
